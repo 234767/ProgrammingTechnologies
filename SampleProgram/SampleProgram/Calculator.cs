b10b0c4f73e6edac9e633e902433ed39de6f5f57
Jakub Pawlak <234767@edu.p.lodz.pl>
2022-03-30 15:10:18 +0200

    removed project 0

--- SampleProgram/SampleProgram/Calculator.cs
+++
@@ -1,9 +0,0 @@
-namespace SampleProgram;
-
-public class Calculator
-{
-    public int Add( int x, int y ) => x + y;
-    public int Subtract( int x, int y ) => x - y;
-    public int Multiply( int x, int y ) => x * y;
-    public int Divide( int x, int y ) => x / y;
-}
