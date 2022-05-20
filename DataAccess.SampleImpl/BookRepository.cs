aef3c6f06f264661d4ef814579ed97956e2cbcaa
Kuba Pawlak <kuba_pawlak@icloud.com>
2022-05-20 20:20:29 +0200

    Fixed BookRepository

--- DataAccess.SampleImpl/BookRepository.cs
+++ DataAccess.SampleImpl/BookRepository.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using AutoMapper;
 using DataAccess.API.Abstractions;
 using DataAccess.API.DTO;
+using DataAccess.Database.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Database;
 
 internal class BookRepository : IBookRepository
 {
     private readonly LibraryDataContext _context;
+    private static readonly IMapper Mapper;
+    
+    static BookRepository()
+    {
+        Mapper = new MapperConfiguration(
+            cfg =>
+            {
+                cfg.CreateMap<IBook, BookDto>();
+                cfg.CreateMap<IBookInfo, BookInfoDto>();
+            } ).CreateMapper();
+    }
 
     public BookRepository(LibraryDataContext context)
     {
@@ -19,69 +33,71 @@ internal class BookRepository : IBookRepository
 
     public async Task CreateAsync(IBook item)
     {
-        if ( _context.Books.Any(b => b.Id == item.Id) )
+        if ( await _context.Books.AnyAsync(b => b.Id == item.Id) )
             return;
-        //_context.Books.Add(item);
-        //if ( !_context.BookInfos.ContainsKey(item.BookInfo.Id) )
-        //{
-        //    _context.BookInfos.Add(item.BookInfo.Id, item.BookInfo);
-        //}
+        BookDto dto = Mapper.Map<IBook, BookDto>( item );
+        await _context.Books.AddAsync( dto );
+        if (!await _context.BookInfos.AnyAsync(info => info.Id == item.BookInfo.Id))
+        {
+            await _context.BookInfos.AddAsync( Mapper.Map<BookInfoDto>(item.BookInfo));
+        }
     }
 
     public async Task<IBook?> GetAsync(string id)
     {
-        return _context.Books.FirstOrDefault(b => b.Id == id);
+        return await _context.Books.FindAsync((BookDto b) => b.Id == id);
     }
 
     public async Task<IEnumerable<IBook>> WhereAsync(Expression<Func<IBook, bool>> predicate)
     {
-        return ( await GetAllAsync() ).Where(predicate.Compile());
+        return await _context.Books.Where(predicate).ToListAsync();
     }
 
     public async Task UpdateAsync(IBook item)
     {
-        foreach ( IBook book in _context.Books )
-        {
-            if ( book.Id != item.Id )
-                continue;
-
-            //_context.Books.Remove(book);
-            //_context.Books.Add(item);
-            return;
-        }
+        await Task.Run(
+            () =>
+            {
+                _context.Books.Update( Mapper.Map<BookDto>( item ) );
+            } );
     }
 
     public async Task UpdateBookInfoAsync(IBookInfo newInfo)
     {
-        //IBookInfo oldInfo = _context._bookInfo[newInfo.Id];
-        //_context._bookInfo[newInfo.Id] = newInfo;
+        IBookInfo? oldInfo = await _context.FindAsync<BookInfoDto>( ( BookInfoDto info ) => info.Id == newInfo.Id);
+        if ( oldInfo is null )
+        {
+            throw new KeyNotFoundException( $"Book info with Id {newInfo.Id} not found" );
+        }
 
-        //foreach ( IBook book in _context.Books.Where(b => b.BookInfo.Equals(oldInfo)) )
-        //{
-        //    book.BookInfo = newInfo;
-        //}
+        BookInfoDto newInfoDto = Mapper.Map<BookInfoDto>( newInfo );
+        _context.BookInfos.Update(newInfoDto);
+
+        foreach (BookDto book in _context.Books.Where(b => b.BookInfo.Equals(oldInfo)))
+        {
+            book.BookInfo = newInfoDto;
+        }
     }
 
     public async Task DeleteAsync(string id)
     {
-        //IBook bookToRemove = _context.Books.First(b => b.Id == id);
-        //_context.Books.Remove(bookToRemove);
-        //if ( _context.Books.All(book => book.BookInfo.Id != bookToRemove.BookInfo.Id) )
-        //{
-        //    _context._bookInfo.Remove(bookToRemove.BookInfo.Id);
-        //}
+        BookDto bookToRemove = await _context.Books.FirstAsync(b => b.Id == id);
+        _context.Books.Remove(bookToRemove);
+
+        if (_context.Books.All(book => book.BookInfo.Id != bookToRemove.BookInfo.Id))
+        {
+            _context.BookInfos.Remove(await _context.BookInfos.SingleAsync(info => info.Id == bookToRemove.BookInfo.Id));
+        }
     }
 
-    public async Task<IEnumerable<IBook>> GetAllAsync() => _context.Books;
+    public async Task<IEnumerable<IBook>> GetAllAsync() => await _context.Books.ToListAsync();
 
-    public async Task<IEnumerable<IBookInfo>> GetAllBookInfoAsync() => null;//_context._bookInfo.Values;
+    public async Task<IEnumerable<IBookInfo>> GetAllBookInfoAsync() => await _context.BookInfos.ToListAsync();
 
     public async Task<IUser?> GetUserWhoLeased(IBook book)
     {
-        return null;
-        //    return _context._events
-        //                   .OfType<ILease>()
-        //                   .FirstOrDefault(l => l.LeasedBook == book && _context._events.OfType<IReturn>().All(r => r.Lease != l))
-        //                   ?.Borrower;
+        return ( await _context.Leases.FirstOrDefaultAsync(
+            l => l.LeasedBook == book && 
+                 _context.Returns.All( r => r.Lease != l ) ) )?.Borrower;
     }
 }
